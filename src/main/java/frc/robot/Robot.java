57fb56858967986d70591c92ced29d2fde61cc92
jcooneyolp <43014370+jcooneyolp@users.noreply.github.com>
2023-02-04 09:59:03 -0800

    i added shuffleboard :)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,13 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 //import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+//import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+//import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+//import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.cameraserver.CameraServer;
+//import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
@@ -26,11 +33,13 @@ public class Robot extends TimedRobot {
   private final WPI_TalonFX talMotorR4 = new WPI_TalonFX(4);
   private final MotorControllerGroup leftMotors = new MotorControllerGroup(talMotorL1, talMotorL2);
   private final MotorControllerGroup rightMotors = new MotorControllerGroup(talMotorR3, talMotorR4);
-
+  
   @Override
   public void robotInit() {
     xbox = new XboxController(0);
     myRobot = new DifferentialDrive(leftMotors, rightMotors);
+    CameraServer.startAutomaticCapture();
+
 
   }
 
@@ -38,5 +47,6 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     //Uses the 2 joysticks on the xboxController to control the left and right side of the tank drive
     myRobot.tankDrive(-xbox.getLeftY(), -xbox.getRightY());
+
   }
 }
